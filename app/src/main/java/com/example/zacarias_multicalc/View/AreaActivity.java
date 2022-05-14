5bfc6bb65881d3bea138ff2d6087b962006c8867
zaccarias <zaccariah23@gmail.com>
2022-05-14 13:04:01 +0800

    code update

--- app/src/main/java/com/example/zacarias_multicalc/View/AreaActivity.java
+++ app/src/main/java/com/example/zacarias_multicalc/View/AreaActivity.java
@@ -11,39 +11,46 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.zacarias_multicalc.Controller.Methods;
+import com.example.zacarias_multicalc.Model.Variables;
 import com.example.zacarias_multicalc.R;
 
 public class AreaActivity extends AppCompatActivity implements View.OnClickListener{
     Button next1,calculate2;
     EditText basetxt,heightxt;
     TextView result2;
-
+    Methods methods=new Methods();
+    Variables var1 =new Variables();
+    double solve;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_area);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_volume);
         getSupportActionBar().hide();
-        next1=findViewById(R.id.Nextone);
-        calculate2=findViewById(R.id.solve);
-        basetxt=findViewById(R.id.base);
-        heightxt=findViewById(R.id.areaheight);
-        calculate2.setOnClickListener(this);
-        result2=findViewById(R.id.equals);
-        next1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent =new Intent(AreaActivity.this,Kinetic.class);
-                startActivity(intent);
-            }
-        });
 
+        //setting id
+        next1 = findViewById(R.id.nextwo);
+        basetxt = findViewById(R.id.base);
+        heightxt = findViewById(R.id.areaheight);
+        calculate2 = findViewById(R.id.solve);
+
+        calculate2.setOnClickListener(this);
+        next1.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.solve:
+            case R.id.calculate:
+                var1.setBase(Double.parseDouble(basetxt.getText().toString()));
+                var1.setHeight1(Double.parseDouble(heightxt.getText().toString()));
+                solve= methods.triangleArea(var1.getBase(),var1.getHeight());
+                result2.setText("result: " + solve);
+                break;
+            case R.id.nextwo:
+                Intent intent = new Intent(AreaActivity.this,Kinetic.class);
+                startActivity(intent);
+                break;
         }
     }
 }
--- app/src/main/java/com/example/zacarias_multicalc/View/VolumeActivity.java
+++ app/src/main/java/com/example/zacarias_multicalc/View/VolumeActivity.java
@@ -11,13 +11,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.zacarias_multicalc.Controller.Methods;
+import com.example.zacarias_multicalc.Model.Variables;
 import com.example.zacarias_multicalc.R;
 
 public class VolumeActivity extends AppCompatActivity implements View.OnClickListener {
+
     Button next,calculate;
     EditText length,width,height;
-    TextView lenghtxt,widthxt,heighttxt,resultxt;
+    TextView resultxt;
     double answer;
+
+    //Calling classes
+    Methods solve = new Methods();
+    Variables variables = new Variables();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,36 +33,31 @@ public class VolumeActivity extends AppCompatActivity implements View.OnClickLis
         getSupportActionBar().hide();
 
         //setting id
-        next =findViewById(R.id.Next);
-        length=findViewById(R.id.lengthtxt);
-        width=findViewById(R.id.widthtxt1);
-        height=findViewById(R.id.Heightxt);
-        calculate=findViewById(R.id.calculate);
-        lenghtxt=findViewById(R.id.length);
-        widthxt=findViewById(R.id.widthtxt1);
-        heighttxt=findViewById(R.id.Heightxt);
-        resultxt =findViewById(R.id.resultkey);
-        Methods solve =new Methods();
-        double l =Double.parseDouble(lenghtxt.getText().toString());
-        double w =Double.parseDouble(widthxt.getText().toString());
-        double h =Double.parseDouble(heighttxt.getText().toString());
-        answer =solve.volumeA(l,w,h);{
-        }
+        next = findViewById(R.id.procceed);
+        length = findViewById(R.id.lengthtxt);
+        width = findViewById(R.id.widthtxt1);
+        height = findViewById(R.id.Heightxt);
+        calculate = findViewById(R.id.calculate);
+        resultxt = findViewById(R.id.resultkey);
+
         calculate.setOnClickListener(this);
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent =new Intent(VolumeActivity.this,AreaActivity.class);
-                startActivity(intent);
-            }
-        });
+        next.setOnClickListener(this);
     }
-    
+
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.calculate:
-                resultxt.setText("result"+answer);
+                variables.setLength(Double.parseDouble(length.getText().toString()));
+                variables.setWidth(Double.parseDouble(width.getText().toString()));
+                variables.setHeight(Double.parseDouble(height.getText().toString()));
+                answer = solve.volumeA(variables.getLength(),variables.getWidth(),variables.getHeight());
+
+                resultxt.setText("result: " + answer);
+                break;
+            case R.id.procceed:
+                Intent intent = new Intent(VolumeActivity.this,AreaActivity.class);
+                startActivity(intent);
                 break;
         }
     }
